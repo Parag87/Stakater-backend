9b0cbb00e1d62f76b9d488809f20f56fe61fbd7d
Parag Mundra <paragmundra@gmail.com>
2021-10-19 16:56:52 +0530

    code changes for hello api

---
+++ src/main/java/com/example/stakater/StakaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.stakater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stakater/StakaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stakater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
